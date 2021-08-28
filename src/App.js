3da0a78c9e37ab86705d511c474eae79e072aed8
Shrinivas Choudhary <shrinivaschowdhary97@gmail.com>
2021-08-28 22:06:43 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
 
 function Floor(props) {
+
+  let upId = props.number + "up";
+  let downId = props.number + "down";
+  let hId = props.number + "floor";
+
   return (
-    <div>
+    <div className="child">
 
       {
-        props.up && <button className="btn">Up</button>
+        
+        props.up && <button className="btn" onClick={props.onClick} id={upId}>UP</button>
       }
       <br/>
       {
-        props.down && <button className="btn">Down</button>
+        props.down && <button className="btn" onClick={props.onClick} id={downId}>DOWN</button>
       }
 
-      <h2 className="floorname">Floor {props.number}</h2>
+      <h2 className="floorname" id={hId}>Floor {props.number}</h2>
 
 
     </div>
@@ -25,15 +30,64 @@ function Floor(props) {
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.lift_top_init = null;
+  }
+
+  componentDidMount() {  
+    let lift = document.getElementById("lift");
+    let liftTop = lift.getBoundingClientRect().top;
+    let viewTop = document.documentElement.scrollTop;
+    this.lift_top_init = liftTop + viewTop;
+
+    this.moveLift("1floor");
+
+  }
+
+
+  handleClick(e) {
+    let button = e.target;
+    let floorId = button.id[0] + "floor";
+
+    this.moveLift(floorId);
+    
+
+  }
+
+  moveLift(dest_floor_id) {
+    let lift = document.getElementById("lift");
+    let liftHeight = lift.getBoundingClientRect().height;
+    
+
+    let floor = document.getElementById(dest_floor_id);
+    let floorTop = floor.getBoundingClientRect().top;
+    let viewTop = document.documentElement.scrollTop;
+    let floorTopAbs = floorTop + viewTop;
+
+    let distance = Math.abs(floorTopAbs - this.lift_top_init) + liftHeight;
+
+    lift.style.top = -distance + "px";
+
+  }
+
   render() {
     return (
-      <div className="divMain">
+      <div>
         <h1>Lift Simulation</h1>
         <br/>
         <br/>
         <br/>
-        <Floor number="2" up={true} down={true}/>
-        <Floor number="1" up={true}/>
+        <div className="container">
+          <Floor number="4" down={true} onClick={(e) => {this.handleClick(e)}}/>
+          <Floor number="3" up={true} down={true} onClick={(e) => {this.handleClick(e)}}/>
+          <Floor number="2" up={true} down={true} onClick={(e) => {this.handleClick(e)}}/>
+          <Floor number="1" up={true} onClick={(e) => {this.handleClick(e)}}/>
+        </div>
+        <div className="lift" id="lift"></div>
+        
+        
 
       </div>
       
